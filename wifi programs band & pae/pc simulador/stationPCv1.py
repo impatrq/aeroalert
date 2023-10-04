d5c942ec6dcc4cd1d1f80b516b321742801453ae
cuccas <manucucca10@gmail.com>
2023-10-04 11:41:13 -0300

    feat: filtrar mensajes

--- wifi programs band & pae/pc simulador/stationPCv1.py
+++ wifi programs band & pae/pc simulador/stationPCv1.py
@@ -1,14 +1,11 @@
 from wireless import Wireless
 import socket, json
 
-
-ssid = 'ESP32'
-password = 'aeroalert'
-
-wire = Wireless()
-wire.connect(ssid, password)
-
 def do_connect():
+    ssid = 'ESP32'
+    password = 'aeroalert'
+    wire = Wireless()
+    wire.connect(ssid, password)
     server_ip = '192.168.4.1'
     server_port = 8000
     addr = socket.getaddrinfo(server_ip, server_port) [0][-1]
@@ -23,20 +20,39 @@ def send_type(tipo, client_socket):
     message = json.dumps(tipo).encode('utf-8')
     client_socket.send(message)
 
-def send_message(client_socket,msg):
-    data = {'mensage': msg}
+def send_message(client_socket, data):
+    
+    # data = {"Piloto": 1,
+    # "Somnolencia": 1,
+    # "Pulso": 94,
+    # "Spo2": 92,
+    # "bloqueo": 1,
+    # "Muerte": 1}
     message = json.dumps(data).encode('utf-8') #.encode('utf-8') para un string se encodea
     
     client_socket.send(message) 
-    print('Mensaje enviado:', message)
+    print('enviado:', message)
 
 def receive_data(client_socket):
     data = client_socket.recv(1024)
-    print('respuesta: ', data)
-    return data
+    print('recibido: ', data)
+    instruccion = json.loads(data.decode('utf-8'))
+
+    return instruccion
+
+
 
 
 client_socket = do_connect()
 send_type('soy_PC', client_socket)
 send_message(client_socket, 'holiwis')
-receive_data(client_socket)
+
+while True:
+    instruccion = receive_data(client_socket)
+    if instruccion == "ATERRIZAR":
+        #send al xplane
+        print("aterrizar")
+    elif instruccion== "NO ATERRIZAR":
+        #send al xplane
+        print("no aterrizar")
+
