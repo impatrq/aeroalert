99a938d3c0b9b4752c84d46de51181b90667e32b
cuccas <manucucca10@gmail.com>
2023-10-21 16:14:40 -0300

    fix: band not finger constantly

--- AES-CTRT-VDB_programs/band/codigos max30102 esp32/main.py
+++ AES-CTRT-VDB_programs/band/codigos max30102 esp32/main.py
@@ -7,21 +7,22 @@ sensor = Pulso()                                # Creo objecto clase pulso
 #pin_conectado = Pin(17, mode=Pin.IN)
 #pin_prendido = Pin(16 , mode=Pin.IN)                                                                                         # pin_prendido.value(1)     porque es el boton y tiene 
 #pin_led = Pin(19, mode=Pin.OUT)                                                                                                     # que estar prendido para que ande
-conectado = pin_conectado.value()
+#conectado = pin_conectado.value()
 client_socket = led_prendido = 0
                                                                                     
 def mediciones():
-    sensor.muestra(pin_prendido)     # Realiza las mediciones
+                    #con pin_prendido se deberia llamar la funcion
+    sensor.muestra()     # Realiza las mediciones
 
 def mostrar():
     utime.sleep(2)
     global client_socket
     while True:
      #   if pin_prendido.value() == 1:
-        print("intentando conectar al AES")
-        client_socket, sta_if = station.do_connect()    
+        print("intentando conectar al AES...")
+        #client_socket, sta_if = station.do_connect()    
         print("conectado al AES", client_socket)
-        station.send_type(client_socket, sta_if)
+        #station.send_type(client_socket, sta_if)
 
         while True:
             #while pin_prendido.value():
@@ -34,8 +35,8 @@ def mostrar():
             conectado = 1 #pin_conectado.value()
             print(f'{beats}, {spo2}, {temp}, {conectado}')
             try:
-                station.send_message(beats, spo2, temp, 
-                                     conectado, client_socket, sta_if)
+                #station.send_message(beats, spo2, temp, 
+                #                     conectado, client_socket, sta_if)
                 print("enviado")
             except:
                 print("No enviado")
