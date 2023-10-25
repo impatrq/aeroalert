7accfd501ab37a610048bd07a89d1d01d6c269e8
cuccas <manucucca10@gmail.com>
2023-10-25 19:30:18 -0300

    feat: playing alerts

--- AES-CTRT-VDB_programs/pc-simulador/PcUART.py
+++ AES-CTRT-VDB_programs/pc-simulador/PcUART.py
@@ -12,23 +12,29 @@ ser = serial.Serial(
 print("connected to: " + ser.portstr)
 
 
+global alarma_sonora_aes_activation, alarma_sonora_aes_alert, alarma_sonora_sleep, alarma_sonora_hypoxia, alarma_sonora_manual_activation, alarma_sonora_test_failed, alarma_sonora_test_pass
 
-global alarma_sonora_hypoxia, alarma_sonora_aes_alert, alarma_sonora_sleep, alarma_sonora_manual_activation
-
-alarma_sonora_hypoxia = alarma_sonora_aes_alert = alarma_sonora_sleep = alarma_sonora_manual_activation = 0
+alarma_sonora_aes_activation = alarma_sonora_aes_alert = alarma_sonora_sleep = alarma_sonora_hypoxia = alarma_sonora_manual_activation = alarma_sonora_test_failed = alarma_sonora_test_pass = 0
 
 def thread_sonidos():
-    global alarma_sonora_hypoxia, alarma_sonora_aes_alert, alarma_sonora_sleep, alarma_sonora_manual_activation
+    global alarma_sonora_aes_activation, alarma_sonora_aes_alert, alarma_sonora_sleep, alarma_sonora_hypoxia, alarma_sonora_manual_activation, alarma_sonora_test_failed, alarma_sonora_test_pass
     while True:
-        if alarma_sonora_hypoxia:
-            playsound()
+        if alarma_sonora_aes_activation:
+            playsound("./imgs_alerts_GUI/alerta_aes_act.mp3")     
         if alarma_sonora_aes_alert:
-            playsound()
+            playsound("./imgs_alerts_GUI/alerta_aes_alert.mp3")
         if alarma_sonora_sleep:
-            playsound()
+            playsound("./imgs_alerts_GUI/alerta_get_up.mp3")
+        if alarma_sonora_hypoxia:
+            playsound("./imgs_alerts_GUI/alerta_hypoxia.mp3")
         if alarma_sonora_manual_activation:
-            playsound()
-
+            playsound("./imgs_alerts_GUI/alerta_manual_act.mp3")
+        if alarma_sonora_test_failed:
+            playsound("./imgs_alerts_GUI/alerta_test_failed.mp3")
+            alarma_sonora_test_failed = 0
+        if alarma_sonora_test_pass:
+            playsound("./imgs_alerts_GUI/alerta_test_pass.mp3")
+            alarma_sonora_test_pass = 0
 _thread.start_new_thread(thread_sonidos, ())
 
 while True:
@@ -45,23 +51,35 @@ while True:
             if read[0] == "info aeropuerto:":
                 xplane.instruccion(read[1])
 
+        #----
+        if read == "alarma_sonora_aes_activation = 1":
+            alarma_sonora_aes_activation = 1
+        elif read == "alarma_sonora_aes_activation = 0":
+            alarma_sonora_aes_activation = 0
+        #----
         elif read == "alarma_sonora_hypoxia = 1":
             alarma_sonora_hypoxia = 1
         elif read == "alarma_sonora_hypoxia = 0"   :
             alarma_sonora_hypoxia = 0
-
+        #----
         elif read == "alarma_sonora_aes_alert = 1":
             alarma_sonora_aes_alert = 1
-        elif read == "alarma_sonora_aes_alert = 0"    :
+        elif read == "alarma_sonora_aes_alert = 0":
             alarma_sonora_aes_alert = 0
-        
+        #-----
         elif read == "alarma_sonora_sleep = 1":
             alarma_sonora_sleep = 1
-        elif read == "alarma_sonora_sleep = 0"     :
+        elif read == "alarma_sonora_sleep = 0":
             alarma_sonora_sleep = 0
-        
+        #----
         elif read == "alarma_sonora_manual_activation = 1":
             alarma_sonora_manual_activation = 1
         elif read == "alarma_sonora_manual_activation = 0":
             alarma_sonora_manual_activation = 0
-            
+        #----
+        elif read == "alarma_sonora_test_failed":
+            alarma_sonora_test_failed = 1
+        #----
+        elif read == "alarma_sonora_test_pass":
+            alarma_sonora_test_pass = 1
+        #----
--- AES-CTRT-VDB_programs/pruebas/Sonido.py
+++ AES-CTRT-VDB_programs/pruebas/Sonido.py
@@ -1,12 +1,14 @@
-#from winsound import PlaySound
-from playsound import playsound
-import time
-import _thread
+from time import sleep
+import sys
 
-def th_func(delay, id):
-    print("hola",delay,id)
-    playsound("C:/Users/manuc/Desktop/github/proyecto/AES-CTRT-VDB_programs/pruebas/sonido-emergency.mp3")
-print("awa")
-i = 1
-_thread.start_new_thread(th_func, (i + 1, i))
-print("owo")
+for i in range(5):
+   print(".", end="", flush=True)
+   sleep(.1)
+print()
+
+
+
+"""
+while True:
+    print(".", end="")
+    sleep(0.1)"""
