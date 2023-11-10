db03d27c2609209f78d00a7aa5b29e8f8fd3be74
cuccas <manucucca10@gmail.com>
2023-11-10 00:16:13 -0300

    clean and rename folders

--- AES-CTRT-VDB_programs/pruebas/global_import.py
+++
@@ -1,6 +0,0 @@
-
-import if_lista
-
-if_lista.a()
-global awa
-print(awa)
--- AES-CTRT-VDB_programs/pruebas/if_lista.py
+++
@@ -1,6 +0,0 @@
-import time
-
-
-hora = time.localtime()
-a = str("%02.0f:%02.0f:%02.0f" %(hora[3],hora[4],hora[5]))
-print(a)
